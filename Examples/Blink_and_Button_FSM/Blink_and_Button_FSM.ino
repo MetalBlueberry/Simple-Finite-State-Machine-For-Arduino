cd5623e657b5d6942312f5ef58acdc5ea1c3aa8a
Victor <victorperezdomingo@gmail.com>
2016-06-16 22:07:07 +0200

    ->Guardar cambios en los ejemplos

--- Examples/Blink_and_Button_FSM/Blink_and_Button_FSM.ino
+++ Examples/Blink_and_Button_FSM/Blink_and_Button_FSM.ino
@@ -35,7 +35,7 @@ FSM_STATE(S5, LedOff, nullptr, nullptr);
 
 //Definir condiciones
 FSM_TRANSITION_CONDITION(Delay1s) {
-  return from->runTime() > 1000;
+  return from.runTime() > 1000;
 }
 FSM_TRANSITION_CONDITION(ButtonDown) {
   return !digitalRead(2);
@@ -65,5 +65,5 @@ void setup() {
 }
 
 void loop() {
-  FSM_RUN;
+  FSM_RUN
 }
--- Examples/Blink_and_Button_Sync_FSM/Blink_and_Button_Sync_FSM.ino
+++ Examples/Blink_and_Button_Sync_FSM/Blink_and_Button_Sync_FSM.ino
@@ -37,16 +37,22 @@ FSM_STATE(S5_wait, nullptr, nullptr, nullptr);
 
 //Definir condiciones
 FSM_TRANSITION_CONDITION(Delay1s) {
-  return from->runTime() > 1000;
+  return from.runTime() > 1000;
 }
 FSM_TRANSITION_CONDITION(Delay100ms) {
-  return from->runTime() > 500;
+  return from.runTime() > 100;
 }
 
-INSTANT
+FSM_TRANSITION_CONDITION(Instant) {
+  return true;
+}
+FSM_TRANSITION_CONDITION_MULTI(Instant) {
+  return true;
+}
 
 //Crear una transiccion doble desde S0 a S1 y S3
-FSM_TRANSITION_MULTI(TSync, Instant, (&S2_wait, &S5_wait), (&S1, &S4));
+FSM_TRANSITION_MULTI_CONDITIONS(TSync, Instant, &S5_wait,&S2_wait);
+FSM_TRANSITION_MULTI_ACTIONS(TSync, &S1, &S4);
 
 FSM_TRANSITION(T0, S0, S1, Instant);
 FSM_TRANSITION(T1, S1, S2, Delay1s);
@@ -59,11 +65,10 @@ FSM_TRANSITION(T5, S5, S5_wait, Delay100ms);
 
 //continuar normalmente
 void setup() {
-  Serial.begin(9600);
   FSM_INITIAL_STATE(S0);
   FSM_INITIAL_STATE(S3);
 }
 
 void loop() {
-  FSM_RUN;
+  FSM_RUN
 }
